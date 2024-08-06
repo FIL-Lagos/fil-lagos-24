ff7c1bad66ec6fbecb8892e2b4aa2f31ca0307db
alabo-excel <iamalaboexcel@gmail.com>
2024-08-06 20:00:58 +0100

    corrections update commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,38 @@ import SpeakersCard from './components/SpeakersCard'
 import Hero from './components/Hero'
 
 function App() {
+  const topics = [
+    "Decentralized Storage Solutions with Filecoin",
+    "AI Integration within the Filecoin Ecosystem",
+    "Sustainable Development through Decentralized Technologies",
+    "Leveraging Filecoin for Digital Economy Transformation",
+    "Innovative Use Cases of Filecoin in Africa",
+    "Opportunities for Startups within the Filecoin Ecosystem",
+    "The Future of Data Economy with Filecoin",
+    "Building on Filecoin Virtual Machine (FVM)",
+    "Empowering Women in Web3 and Blockchain Technologies",
+    "Funding and Grant Opportunities in the Filecoin Ecosystem",
+    "Practical Applications of Decentralized Storage in Various Industries",
+    "Connecting the African Tech Community through Decentralization",
+    "Enhancing Project Security and Efficiency with Filecoin",
+    "Filecoin's Role in the Evolving Digital Landscape",
+    "Panel Discussions with Industry Leaders and Innovators",
+    "The FVM Imaginarium: Developer Tooling, Hackathons and other Opportunities for Builders",
+    "Hacking the Filecoin Ethereum Virtual Machine (FEVM)",
+    "The markets & data layer on Filecoin",
+    "Deploying the InterPlanetary File System (IPFS) in space",
+    "Integration with DeFi",
+    "Filecoin cross-chain interoperability",
+    "Building Wallets",
+    "NFT Storage"
+  ];
 
   return (
     <Fragment>
       <HeaderNav />
       <main className='text-white'>
         <Hero />
-        <div className='lg:flex lg:pt-40 pt-16 pb-20 bg-[#131744] justify-between'>
+        <div id='about' className='lg:flex lg:pt-40 pt-16 pb-20 bg-[#131744] justify-between'>
           <div className='my-auto lg:order-1 lg:w-[60%] lg:p-10 p-6'>
             <p className='text-[30px]'>ABOUT</p>
             <h1 className='lg:text-[50px] text-5xl font-bold my-2'>FIL LAGOS</h1>
@@ -22,7 +47,7 @@ function App() {
             <img src="/images/about-img.png" alt="" />
           </div>
         </div>
-        <section className='lg:p-20 p-6 bg-[#090B22]'>
+        <section id='sponsors' className='lg:p-20 p-6 bg-[#090B22]'>
           <div>
             <p className='text-[30px]'>MEET OUR</p>
             <h1 className='lg:text-[50px] text-5xl font-bold my-3'>Sponsors</h1>
@@ -33,11 +58,13 @@ function App() {
           </div>
           <img className='mx-auto my-10 w-32' src="/images/sponsors/img3.png" alt="" />
           <img className='mx-auto w-80' src="/images/sponsors/img4.png" alt="" />
-          <div className='text-center p-8'>
-            <button className="bg-[#0A48C0] text-white rounded-full p-3 px-10 border-2 border-[#52F4FE]">BECOME A SPONSOR</button>
-          </div>
+          <a href="mailto:info@filecoin.africa">
+            <div className='text-center p-8'>
+              <button className="bg-[#0A48C0] text-white rounded-full p-3 px-10 border-2 border-[#52F4FE]">BECOME A SPONSOR</button>
+            </div>
+          </a>
         </section>
-        <section className='lg:p-20 p-6 bg-[#090B22]'>
+        {/* <section className='lg:p-20 p-6 bg-[#090B22]'>
           <div>
             <p className='text-[30px] text-[#F1BE07]'>EXPLORE</p>
             <h1 className='lg:text-[50px] text-5xl  font-medium my-3'>FIL Lagos Event</h1>
@@ -64,8 +91,8 @@ function App() {
             </div>
             <img className='h-10 my-1' src="/images/launch-link-open.png" alt="" />
           </div>
-        </section>
-        <section className='lg:p-20 p-6 bg-[#131744]'>
+        </section> */}
+        <section id='speakers' className='lg:p-20 p-6 bg-[#131744]'>
           <div>
             <p className='text-[30px] text-[#F1BE07]'>DISCOVER</p>
             <h1 className='lg:text-[50px] text-5xl font-medium my-3'>FIL Lagos Speakers</h1>
@@ -76,28 +103,33 @@ function App() {
             <SpeakersCard />
             <SpeakersCard />
           </div>
+          <div className='flex justify-center mt-6'>
+            <a href="https://bit.ly/fil-speaker" target='_blank'>
+              <button className="bg-[#0A48C0] text-white rounded-full p-3 px-10 border-2 border-[#52F4FE]">BECOME A SPEAKER</button>
+            </a>
+          </div>
         </section>
-        <section className='lg:p-20 p-6 bg-[#090B22]'>
+        <section id='agenda' className='lg:p-20 p-6 bg-[#090B22]'>
           <div>
             <p className='text-[30px] text-[#F1BE07]'>JOIN US</p>
             <h1 className='lg:text-[50px] text-5xl font-medium my-3'>FIL Lagos Schedule</h1>
           </div>
           <div className='my-4'>
             <button className='bg-[#0CDD92] border-2 border-[#0CDD92] p-3 rounded-xl lg:w-auto w-full my-1 px-8'>Day1 , 3rd September</button>
-            <button className='border-2 border-[#0CDD92] p-3 rounded-xl lg:ml-4 lg:w-auto my-1 w-full px-8'>Day2 , 4th September</button>
+            {/* <button className='border-2 border-[#0CDD92] p-3 rounded-xl lg:ml-4 lg:w-auto my-1 w-full px-8'>Day2 , 4th September</button> */}
           </div>
-          <div className='border-2 my-8 border-[#3679FB] p-10 rounded-[40px] lg:flex justify-between'>
+          {topics.map((topic, index) => <div key={index} className='border-2 my-8 border-[#3679FB] p-10 rounded-[40px] lg:flex justify-between'>
             <div>
-              <p className='text-[#F1BE07] text-[24px]'>9am</p>
-              <p className='text-[14px]'>The zone park, Gbagada
-                first hall</p>
+              {/* <p className='text-[#F1BE07] text-[24px]'>9am</p> */}
+              <p className='text-[20px]'>The Zone Tech</p>
             </div>
-            <div className='lg:w-[60%]'>
-              <p className='text-[#0CDD92] my-1 text-[30px]'>Door opens, Welcome to FIL Lagos, settle and confirm ticket</p>
-              <p className='text-[14px]'>Join us as we kick start the event  and get busy with building and understanding more about Filecoin visual machine</p>
+            <div className='lg:w-[70%]'>
+              <p className='text-[#0CDD92] my-1 text-[30px]'>{topic}</p>
+              {/* <p className='text-[14px]'>Join us as we kick start the event  and get busy with building and understanding more about Filecoin visual machine</p> */}
             </div>
-          </div>
-          <div className='border-2 my-8 border-[#3679FB] p-10 rounded-[40px] lg:flex justify-between'>
+          </div>)}
+
+          {/* <div className='border-2 my-8 border-[#3679FB] p-10 rounded-[40px] lg:flex justify-between'>
             <div>
               <p className='text-[#F1BE07] text-[24px]'>9:30am</p>
               <p className='text-[14px]'>The zone park, Gbagada
@@ -107,7 +139,7 @@ function App() {
               <p className='text-[#0CDD92] my-1 text-[30px]'>Registration and materials collection </p>
               <p className='text-[14px]'>Learn with us how to make money from filecoin and also explore the numerous financial opportunities in the ecosystem</p>
             </div>
-          </div>
+          </div> */}
         </section>
       </main>
       <FooterNav />
--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -10,12 +10,20 @@ const HeaderNav = () => {
   return (
     <header className="bg-[#131744] p-4 lg:px-10 flex justify-between items-center">
       <img src="/images/logo.png" alt="Logo" className="h-8" />
-      <div className="hidden md:flex text-[#F6FBFF] w-[40%] justify-between text-sm">
-        <p>ABOUT</p>
-        <p>SPONSORS</p>
-        <p>EVENTS</p>
-        <p>SPEAKERS</p>
-        <p>AGENDA</p>
+      <div className="hidden md:flex text-[#F6FBFF] w-[35%] justify-between text-sm">
+        <a href="#about">
+          <p>ABOUT</p>
+        </a>
+        <a href={'#sponsors'}>
+          <p>SPONSORS</p>
+        </a>
+        {/* <p>EVENTS</p> */}
+        <a href="#speakers">
+          <p>SPEAKERS</p>
+        </a>
+        <a href="#agenda">
+          <p>AGENDA</p>
+        </a>
       </div>
       <a href="https://lu.ma/FIL-LAGOS" target='_blank'>
         <button className="hidden md:block bg-[#0A48C0] text-white rounded-full p-2 px-10 border-2 border-[#52F4FE]">
@@ -42,14 +50,23 @@ const HeaderNav = () => {
       </div>
       {isMenuOpen && (
         <div className="absolute top-16 left-0 py-10 w-full bg-[#131744] text-[#F6FBFF] flex flex-col items-center md:hidden">
-          <p className="py-2">ABOUT</p>
-          <p className="py-2">SPONSORS</p>
-          <p className="py-2">EVENTS</p>
-          <p className="py-2">SPEAKERS</p>
-          <p className="py-2">AGENDA</p>
-          <button className="bg-[#0A48C0] text-white rounded-full p-2 px-10 border-2 border-[#52F4FE] my-2">
-            REGISTER
-          </button>
+          <a href="#about">
+            <p onClick={() => toggleMenu()} className="py-2">ABOUT</p>
+          </a>
+          <a href={'#sponsors'}>
+            <p onClick={() => toggleMenu()} className="py-2">SPONSORS</p>
+          </a>
+          <a href="#speakers">
+            <p onClick={() => toggleMenu()} className="py-2">SPEAKERS</p>
+          </a>
+          <a href="#agenda">
+            <p onClick={() => toggleMenu()} className="py-2">AGENDA</p>
+          </a>
+          <a href="https://lu.ma/FIL-LAGOS" target='_blank'>
+            <button className="bg-[#0A48C0] text-white rounded-full p-2 px-10 border-2 border-[#52F4FE] my-2">
+              REGISTER
+            </button>
+          </a>
         </div>
       )}
     </header>
